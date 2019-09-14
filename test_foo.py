cb6ba19ce620b67ef838884423b3efe56b6145b2
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-14 09:36:08 +0200

    Make test failing with `spec=Bar` attribute

--- test_foo.py
+++ test_foo.py
@@ -1,11 +1,11 @@
 from unittest.mock import Mock
 
-from foo import Foo
+from foo import Foo, Bar
 
 
 class TestFoo:
     def test_should_delegate_to_bar_when_run_is_called(self):
-        mocked_bar = Mock()
+        mocked_bar = Mock(spec=Bar)
         foo = Foo(mocked_bar)
         foo.run()
         mocked_bar.some_method.assert_called_once_with()
